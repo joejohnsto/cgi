6834e0415a3a78c9dfd36ae6026d0514ffa24c18
joejohnsto <joejohnsto@gmail.com>
2020-04-24 13:02:22 -0700

    Updated post lesson

--- cgi-bin/cgi_sums.py
+++ cgi-bin/cgi_sums.py
@@ -4,6 +4,15 @@ import cgitb
 
 cgitb.enable()
 
+form = cgi.FieldStorage()
+listval = form.getlist('operand')
+try:
+    listnums = [int(_) for _ in listval]
+    result = sum(listnums)
+    body = f"The sum is {result}!"
+except (ValueError, TypeError):
+    body = "Please provide integer operands!"
+
 print("Content-type: text/plain")
 print()
-print("Your job is to make this work")
+print(body)
